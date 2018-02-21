f3cc6fb2d82e871253f73d00de04285f1f2a3cb4
frankwichw <frankwichw@gmail.com>
2018-02-21 14:45:38 -0500

    modal added

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,8 +1,19 @@
 var friends = [
     {
         "name": "Bink",
-        "photo": "photohere",
-        "scores": []
+        "photo": "http://78.media.tumblr.com/46959895ad010fd289901d7a73c0a19e/tumblr_o8f36vPFYJ1v6fl6fo1_1280.jpg",
+        "scores": [
+            "4",
+            "1",
+            "2",
+            "5",
+            "5",
+            "3",
+            "2",
+            "1",
+            "1",
+            "1"
+        ]
     }
 ];
 
